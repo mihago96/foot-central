c73d45fa8feb50093fea05c644cb3aaf3c5cbc51
mihago96 <hei.mihago.2@gmail.com>
2025-05-05 15:20:47 +0300

    chore(app): Add @ComponentScan in CentralApplication class.

--- src/main/java/org/prog3/central_api/CentralApiApplication.java
+++ src/main/java/org/prog3/central_api/CentralApiApplication.java
@@ -2,8 +2,10 @@ package org.prog3.central_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"org.prog3.central_api","org.prog3.central_api.controller"})
 public class CentralApiApplication {
 
     public static void main(String[] args) {
