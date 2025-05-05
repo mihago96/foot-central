54473d017e5d7df09cf175ba2714000fddbd59d0
mihago96 <hei.mihago.2@gmail.com>
2025-05-05 09:11:15 +0300

    chore: Initialisation of the project.

---
+++ src/test/java/org/prog3/central_api/CentralApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.prog3.central_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
