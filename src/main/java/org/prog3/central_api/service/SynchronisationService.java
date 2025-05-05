0740ed7be18daeb06aa384b100651db8059ff1b2
mihago96 <hei.mihago.2@gmail.com>
2025-05-05 20:13:16 +0300

    feat(service): Add all services templates.

---
+++ src/main/java/org/prog3/central_api/service/SynchronisationService.java
@@ -0,0 +1,9 @@
+package org.prog3.central_api.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class SynchronisationService {
+}
